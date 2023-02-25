fd541c0dc1e378854333715e853feac97ed46431
Lukario45 <lukario45@mcthunder.net>
2023-02-25 03:38:16 -0500

    Initial Commit. Mod should Compile and exist in name only ingame.

---
+++ src/main/java/net/mcthunder/Main.java
@@ -0,0 +1,10 @@
+package net.mcthunder;
+
+/**
+ * Created by Lukario45 on ${DATE}.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/net/mcthunder/VillagesArise/blocks/BlockTownHall.java
@@ -0,0 +1,7 @@
+package net.mcthunder.VillagesArise.blocks;
+
+/**
+ * Created by Lukario45 on 2/24/2023.
+ */
+public class BlockTownHall {
+}
